833ce152a42915ce0c0af191ddde20b97df7e48d
FrancyelZelling <zellingf@gmail.com>
2020-11-27 13:28:59 -0300

    Added AddTodo Component & Functionality

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <form @submit="addTodo">
+      <input type="text" name="title" v-model="title" placeholder="Add Todo" />
+      <input type="submit" value="Submit" class="btn" />
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { v4 } from "uuid";
+import Vue from "vue";
+export default Vue.extend({
+  name: "AddTodo",
+  data() {
+    return {
+      title: "",
+    };
+  },
+  methods: {
+    addTodo(e: HTMLFormElement) {
+      e.preventDefault();
+      const newTodo = {
+        id: v4(),
+        title: this.title,
+        completed: false,
+      };
+      this.$emit("add-todo", newTodo);
+
+      this.title = "";
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+form {
+  display: flex;
+}
+
+input {
+  &[type="text"] {
+    flex: 10;
+    padding: 5px;
+  }
+
+  &[type="submit"] {
+    flex: 2;
+  }
+}
+</style>
